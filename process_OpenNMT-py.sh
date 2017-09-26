eb8fde36cea97ae5a5074ea26e40fac136007863
henrye <c.henry.elder@gmail.com>
2017-09-26 11:39:36 +0100

    start using opennmt

---
+++ process_OpenNMT-py.sh
@@ -0,0 +1,1 @@
+ python ~/downloads/OpenNMT-py/preprocess.py -train_src data/trainset-source.tok.2000.bpe -train_tgt data/trainset-target.tok.2000.bpe -valid_src data/devset-source.tok.2000.bpe -valid_tgt data/devset-target.tok.2000.bpe -save_data data/opennmt
